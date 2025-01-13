a417bc86fdba8619ec2e46ca4e3b34d6cf148db8
andredp.developer <andredp.developer@gmail.com>
2025-01-13 13:05:05 +0200

    finished asteroids

--- main.py
+++ main.py
@@ -3,11 +3,33 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shoot import *
 
 def main():
     pygame.init()
     # new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # 60 FPS
+    clock = pygame.time.Clock()
+    # delta
+    dt = 0
+    # create pygame groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    Shoot.containers = (shots, updateable, drawable)
+
+    asteroidField = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     #game loop
     while True:
@@ -18,6 +40,29 @@ def main():
             
         #fill with black
         screen.fill((1,1,1))
+
+        #move player
+        for sprite in updateable:
+            sprite.update(dt)
+        player.move(dt)
+
+        #draw player
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        #asteroids
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                exit()
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        # 60 FPS (1/60th of a second)
+        dt = clock.tick(60)
+        dt /= 1000  # convert to second
 
         # display update
         pygame.display.flip()  
